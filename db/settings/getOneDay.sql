40836877d74bd72dc94549ccb27f387c7f13a2ed
distoneman <distoneman@gmail.com>
2020-06-08 12:40:45 -0600

    changed sort order

--- db/settings/getOneDay.sql
+++ db/settings/getOneDay.sql
@@ -1,3 +1,3 @@
 SELECT * FROM k_schedule
 WHERE k_slots_id = $(id) AND sched_status != 'Replaced'
-ORDER BY cust_name;
+ORDER BY cust_name, sched_id;
--- db/settings/updateConfirmed.sql
+++ db/settings/updateConfirmed.sql
@@ -6,4 +6,5 @@ WHERE sched_id = $(id);
 SELECT * FROM k_schedule
 WHERE sched_date = $(schedDate) 
 AND animal_type = $(animalType) 
-AND sched_status != 'Replaced';
+AND sched_status != 'Replaced'
+ORDER BY cust_name, sched_id;
